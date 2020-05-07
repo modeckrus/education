61e2ebd52802f87659bcde2a0b02735a62f99944
modeckrus <modeckrus.official.page@gmail.com>
2020-05-07 15:04:51 +0300

    add Firestore for Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -53,13 +54,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    Firestore.instance.collection('test').document('testPlatform').setData({
+      'FromWeb':'ok'
     });
   }
 
